4bce79a81316d2b60dce49509717e72b3c30c2fc
Jason <jason27chan@gmail.com>
2020-11-20 21:40:03 -0500

    got images to work

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,9 +1,12 @@
 <template>
     <div id="content-container">
         <div class="therapist-card" v-for="(therapist, index) in therapists" v-bind:key="index">
-            {{ therapist.name }}
-            {{ therapist.insurance }}
-            {{ therapist.good }}
+            <img :src="therapist.imgLink" alt="therapist picture" class="propic" />
+            <div> 
+                <p>Name: {{therapist.name}}</p>
+                <p>Valid Insurance: {{therapist.insurance}}</p>
+                <p>Accepting appointments?: {{therapist.good}}</p>
+            </div>
         </div>
     </div>
 </template>
@@ -14,19 +17,22 @@ export default {
         return {
             therapists: [
                 {
-                    name: "angelina",
-                    insurance: "blue",
-                    good: "yes"
+                    name: "Drew",
+                    insurance: "blue shield",
+                    good: "yes",
+                    imgLink: "http://www.newdesignfile.com/postpic/2013/07/shutterstock-stock-photography_376128.jpg"
                 },
                 {
-                    name: "angelina",
-                    insurance: "blue",
-                    good: "yes"
+                    name: "Angelina",
+                    insurance: "green shield",
+                    good: "yes",
+                    imgLink: "http://www.newdesignfile.com/postpic/2013/07/free-stock-photos-people_376135.jpg"
                 },
                 {
-                    name: "angelina",
-                    insurance: "blue",
-                    good: "yes"
+                    name: "Frank",
+                    insurance: "purple shield",
+                    good: "yes",
+                    imgLink: "https://101clipart.com/wp-content/uploads/02/Reading%20Child%20Clipart%2002.gif"
                 }
             ]
         }
@@ -36,6 +42,15 @@ export default {
 
 <style scoped>
 #content-container {
-    background-color: #a0c4ff;
+    background-color: #f1fafd;
+    text-align:center;
+}
+
+.therapist-card {
+    margin-top: 20px;
+}
+
+.propic {
+    max-width:100px;
 }
 </style>
