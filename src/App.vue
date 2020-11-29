5329d270f87ee953bf891a7b206bd4778abad9f8
Jason <jason27chan@gmail.com>
2020-11-28 20:50:56 -0500

    remove header footer placeholders

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,16 @@
 <template>
   <div>
-    <Header />
     <Content />
-    <Footer />
   </div>
 </template>
 
 <script>
-import Header from './components/Header'
-import Footer from './components/Footer'
 import Content from './components/Content'
 
 export default {
   name: 'App',
   components: {
-    Header,
-    Content,
-    Footer,
+    Content
   }
 }
 </script>
