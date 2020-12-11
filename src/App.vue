ccd7ed8909d300b0d9315c9a87abc4b3de48724a
Jason <jason27chan@gmail.com>
2020-12-10 19:44:39 -0500

    start over using vuematerial webpack start

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,23 @@
 <template>
-  <div>
-    <Header />
-    <Content />
-    <Footer />
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
   </div>
 </template>
 
 <script>
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Content from './components/Content'
-
 export default {
-  name: 'App',
-  components: {
-    Header,
-    Content,
-    Footer,
-  }
+  name: 'App'
 }
 </script>
 
 <style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
